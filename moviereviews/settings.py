d32b3b672ccbfa68c99b71a57b61c38691d18525
Jarodriguo <104769288+Jarodriguo@users.noreply.github.com>
2023-11-01 10:28:18 -0500

    Nueva ip externa

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '2hl5684eyz&6jd+3-rx=vg8enjb+d=f06p^lt^z%%nel1qds_q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['35.222.65.79']
+ALLOWED_HOSTS = ['34.132.208.105']
 
 
 # Application definition
